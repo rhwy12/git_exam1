627d3f227e989ab6215cf24d11d8b0bef599560c
Kim, kiwon <kiwon1117@gmail.com>
2021-09-28 09:23:08 +0900

    201733001-4

--- src/git_exam1/Hello.java
+++ src/git_exam1/Hello.java
@@ -3,9 +3,10 @@ package git_exam1;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("IT융합자율학부");
-		System.out.println("201014099");
-		System.out.println("홍길동");
+		System.out.println("소속: IT융합자율학부");
+		System.out.println("학번: 201014099");
+		System.out.println("이름: 홍길동");
 	}
 
 }
+
